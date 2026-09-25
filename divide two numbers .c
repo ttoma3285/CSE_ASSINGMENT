edeb290b775e8bc29e87a25d0d705076d2100228
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:26:05 +0600

    Add program to divide two numbers

---
+++ divide two numbers .c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ float a,b,Result;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+    printf("enter the value if b:");
+    scanf("%f",&b);
+    Result=a/b;
+    printf("The divide of two value:%.3f",Result);
+    
+    return 0;
+}
