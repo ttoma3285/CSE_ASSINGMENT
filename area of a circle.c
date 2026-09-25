5b2c63a2eb92c38533f9c1ea24750bf13f99b054
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:39:56 +0600

    Add files via upload

---
+++ area of a circle.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main() {
+    
+   float r,Area;
+    float pi;
+    printf("Enter the value of r:");
+    scanf("%f",&r);
+    printf("Enter the value of pi:");
+    scanf("%f",&pi);
+    Area=pi*r*r;
+    printf("Area of circle value=%.3f",Area);
+}
+    
