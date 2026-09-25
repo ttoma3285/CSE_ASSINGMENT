3ad6858c3b8929b1b392740d43bdfd57394c4684
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:43:43 +0600

    Add files via upload

---
+++ greater between two numbers..c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main() {
+
+    int a,b;
+    printf("enter the number a : ");
+    scanf("%d",&a);
+    printf("enter the number b : ");
+    scanf("%d",&b);
+    
+    if(a>b){
+        printf("%d is greater",a);
+    }
+        else if(a<b){
+                printf("%d is greater",b);
+    
+        }
+    else{
+        printf("the number is equel");
+    }
+    
+}
+
