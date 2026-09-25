28a167cdb6a309c8b68ca54bf9af54a3e6f349fd
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:23:00 +0600

    Add program to subtract two numbers

---
+++ subtract two numbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+ float a,b,subtract;
+    printf("enter the value of a:");
+    scanf("%f",&a);
+    printf("enter the value if b:");
+    scanf("%f",&b);
+    subtract=a-b;
+    printf("The subtract of two value:%.3f",subtract);
+    
+    return 0;
+}
