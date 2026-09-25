f65a7aa30799d27c047dda9e8f5b61bc0e0e61cc
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:40:14 +0600

    Add files via upload

---
+++ area of a rectangle.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+   float length,width, Area;
+    printf("Enter the length:");
+    scanf("%f",&length);
+    printf("Enter the width:");
+    scanf("%f",&width),
+    Area=length*width;
+    printf("Area of Rectangle:%.2f",Area);
+}
+ 
