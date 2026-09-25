4d3a3ccb2679fd98a2dd2b711a56800164a2d19c
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:27:17 +0600

    Add program to calculate remainder of two numbers

---
+++ remainder of two numbers .c
@@ -0,0 +1,14 @@
+
+#include <stdio.h>
+
+int main() {
+ int a,b,Result;
+    printf("enter the value of a:");
+    scanf("%d",&a);
+    printf("enter the value if b:");
+    scanf("%d",&b);
+    Result=a % b;
+    printf("The  remainder of two value:%d",Result);
+    
+    return 0;
+}
