fb9f2788545e536cd714652bcdb4bfbcb2c6a0b9
sumiya akter Toma <ttoma3285@gmail.com>
2026-09-26 00:41:25 +0600

    Add files via upload

---
+++ area of a triangle.c
@@ -0,0 +1,12 @@
+
+   #include <stdio.h>
+
+int main() {
+   float base,height, Area;
+    printf("Enter the base:");
+    scanf("%f",&base);
+    printf("Enter the height:");
+    scanf("%f",&height);
+    Area=0.5*base*height;
+    printf("Area of triangle:%.2f",Area);
+}
